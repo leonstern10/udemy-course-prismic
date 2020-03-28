fc67b5605b2b7a39ef5dc91b207caf68b2173034
unknown <leoonns10>
2020-03-28 15:01:49 +0300

    button rendering not inished

--- src/components/listBox.js
+++ src/components/listBox.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import ListsGrid from './listsGrid';
-import {RichText} from 'prismic-reactjs';
+import {RichText, KeyText} from 'prismic-reactjs';
 import styled from 'styled-components';
+import {Link} from 'gatsby';
 
 
 const ListBoxWrapper = styled.section `
-padding: 20px;
+padding: 40px 0;
 background: #fffff;
 border-bottom: 1px solid #CACACA;
-border-top: 1px solid #CACACA;
+`;
 
+
+const Button = styled.div ` 
+background: #8461C9;
+display: inline-block;
+border-radius: 4px;
+padding: 4px 8px;
+cursor: pointer;
 
 `
 
 
-const ListBox = ({title, content}) => {
+const ListBox = ({title, content, buttonLabel, buttonDestination}) => {
     return ( 
     <ListBoxWrapper>
         <RichText render={title}/>
         <RichText render={content}/>
+        <Button>
+            <Link render ={buttonLabel}/>
+        </Button>
     </ListBoxWrapper>
 )
 }
--- src/components/listsGrid.js
+++ src/components/listsGrid.js
@@ -9,13 +9,21 @@ const ListsGridWrapper = styled.section `
     max-width: 800px;
     margin: 0 auto;
 
-
-h2{
+    h3{
     font-family: 'Raleway', sans-serif;
     font-size: 20px;
+    padding-top: 54px;
+    padding-bottom: 21px;
     border-bottom: 1px solid #CACACA;
+    margin-bottom: 0;
 }
-`
+p{
+    font-family: 'Lat
+    margin-block-start: 0px;
+
+}`;
+
+
 const ListsGrid = ({title, lists}) => {
     return (
         <ListsGridWrapper>
@@ -26,6 +34,8 @@ const ListsGrid = ({title, lists}) => {
                     <ListBox 
                     title={list.article_title}
                     content={list.content}
+                    buttonLabel={list.button_label}
+                    buttonDestination={'/${list.button_destination._meta.uid}'}
                     key={i}/>
                         
                    
