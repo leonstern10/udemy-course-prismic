171e6ed44d965eae4939e80feff799294c506e91
unknown <leoonns10>
2020-04-10 23:51:20 +0300

    add comparison box 1

--- src/components/comparisonBox.js
+++ src/components/comparisonBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { RichText } from 'prismic-reactjs';
 import styled from 'styled-components';
 
-const ComparisonBox = () => {
+const ComparisonBox = (popularTag, feature1, feature2, feature3, price, readMore, serviceDescription, serviceLogo, webLabel, webLink ) => {
 return(
     <h1>
         ho
