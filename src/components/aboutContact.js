8c496400a0a48726da7fe94629fe2c4607ed140c
unknown <leoonns10>
2020-04-21 16:13:36 +0300

    styling about hp

--- src/components/aboutContact.js
+++ src/components/aboutContact.js
@@ -2,6 +2,7 @@ import React from 'react';
 import RichText from './richText';
 import styled from 'styled-components';
 import { Link } from "gatsby";
+import styles from "../css/aboutContact.module.css"
 
 
 const AboutWrapper = styled.section `
@@ -12,27 +13,33 @@ const AboutContact = ({aboutTitle, aboutContent, contacTitle, contactContent, bu
     return(
 
         
-        <div>
+        <div className={styles.contain}>
+            <hr className={styles.line}/>
+        <div className={styles.aboutWrapper}>
         <div>
         <RichText render={aboutTitle}/>
         </div>
-        <div>
+        <div className={styles.aboutContent}>
         <RichText render={aboutContent}/>
         </div>
+        </div>
         <div>
-        
+        <div className= {styles.contactWrap}>
+            <div>
         <RichText render={contacTitle}/>
-       
         </div>
         <div>
         <RichText render={contactContent}/>
         </div>
-         <div>
+        <div>
         <Link to ={contactLink}>
             {buttonLabel}
         </Link>
         </div>
+        </div>
         
+         
+        </div>
         
         </div>
 
