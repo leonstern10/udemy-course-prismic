edc468606572c2c736389fe8081b39590b664587
unknown <leoonns10>
2020-04-23 13:13:34 +0300

    fixing styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,56 +1,37 @@
-// import React from "react"
-// import { useStaticQuery, graphql, Link } from "gatsby"
-// import styles from "../css/footer.module.css"
+import React from "react";
+import {Link} from "gatsby";
+import styles from "../css/footer.module.css";
+import links from "../utils/footerLinks"
 
-// const Footer = () => {
-//   const data = useStaticQuery(graphql`
-//     {
-//       prismic {
-//         allFooters {
-//           edges {
-//             node {
-//               aviso
-//               copyright
-//               footer {
-//                 label
-//                 link {
-//                   ... on PRISMIC_Footerpages {
-//                     _meta {
-//                       uid
-//                     }
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `)
-//   return <div className={styles.footerWrap}>
-//       <div className={styles.footerLinks}>
-        
-//       {data.prismic.allFooters.edges[0].node.footer.map((link) => {
-//                   return (
-//                     <div className={styles.footerLink} key={link.link._meta.uid}>
-//                       <Link to={`/${link.link._meta.uid}`}>{link.label}</Link>
-//                     </div>
-//                   )
-//                 })}
-//               </div>
-//               {/* <div className={styles.aviso}>
-//                 <p>
-//                   Aviso: Los redactores de eltop10 no están asociados con los
-//                   fabricantes de los productos recomendados. Sin embargo,
-//                   publicamos enlaces de diferentes tiendas online que nos
-//                   aprovisionan. 
-//                 </p>
-//                 <p className ={styles.pia}>PIA is the best!</p>
-//               </div>
-//               <div className={styles.copyright}>
-//                 <p>Copyright © 2020 eltop10</p>
-//               </div> */}
-//   </div>
-// }
+const Footer = () => {
+  
+  return (<div className={styles.footerWrap}>
+      <div className={styles.footerLinks}>
 
-// export default Footer
+        {links.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link to={item.path}>{item.text}</Link>
+              </li>
+            )
+          })}
+        </div>
+      
+              <div className={styles.aviso}>
+                <p>
+                  Aviso: Los redactores de eltop10 no están asociados con los
+                  fabricantes de los productos recomendados. Sin embargo,
+                  publicamos enlaces de diferentes tiendas online que nos
+                  aprovisionan. 
+                </p>
+                <p className ={styles.pia}>PIA is the best!</p>
+              </div>
+              <div className={styles.copyright}>
+                <p>Copyright © 2020 eltop10</p>
+              </div>
+  
+  </div>
+  )
+}
+
+export default Footer
---
+++ src/utils/footerLinks.js
@@ -0,0 +1,14 @@
+export default [
+    
+    
+    {
+      path: "/politica-de-privacidad",
+      text: "Politica de privacidad",
+    },
+    {
+        path: "/terminos-de-uso",
+        text: "Terminos de uso",
+      },
+    
+   
+  ]
